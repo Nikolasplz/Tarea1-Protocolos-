740bd24bd1781c632d68ad4f61f05d7a489738e4
Fabian Arias <168220938+fabinsky9@users.noreply.github.com>
2024-05-02 15:44:37 -0400

    Update funciones.cpp

--- funciones.cpp
+++ funciones.cpp
@@ -2,11 +2,29 @@
 #include "protocolo.h"
 
 #include <stdio.h>
+#include <string.h>
 
 void menuEmisor(){
 
-    
+int empaquetar (Protocolo & proto){
+                        // 3 bits                    5 bits
+    proto.trama[0] = (proto.CMD & 0x07)| ((proto.LNG & 0x1F)<<3);
+    memcpy(&trama[1], DATA, LNG); 
+}
 
+int fcs(BYTE * arr,int tam){
+    int sum_bits=0;
+    for (int i = 0; i < tam; i++) // recorrer el arreglo
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            sum_bits += (arr[i] >> j) & 0x01;
+        }
+        
+    }
+    return sum_bits;
+}
+    
     // Mostrar el menu
         printf("\n===== ENVIO DE MENSAJES - EMISOR  =====\n");
         printf("Opcion 1 - Cerrar el programa receptor\n");
